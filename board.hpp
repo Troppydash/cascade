52a439e6178cc55acd067dd417863ba28b1b92bd
troppydash <troppydash.developer@gmail.com>
2026-05-01 00:44:06 +0800

    better board

--- board.hpp
+++ board.hpp
@@ -10,7 +10,7 @@ constexpr int HEIGHT = 12;
 constexpr int SIZE = 8;
 constexpr int SIDES = 2;
 constexpr int DROPS = 4 * 2;
-constexpr int DRAW_LENGTH = 200;
+constexpr int DRAW_LENGTH = 300;
 constexpr int PLACE_SIZE = 3;
 
 constexpr int WHITE = 0;
@@ -18,7 +18,8 @@ constexpr int BLACK = 1;
 constexpr int DRAW = 2;
 constexpr int NONE = 3;
 
-struct move {
+struct move
+{
     constexpr static int UP = -SIZE;
     constexpr static int DOWN = SIZE;
     constexpr static int LEFT = -1;
@@ -31,8 +32,6 @@ struct move {
 
     // expand
     bool expand;
-    // original height
-    int height;
     // src square
     int square;
     // move dir
@@ -40,47 +39,51 @@ struct move {
 
     int _score = 0;
 
-    std::array<int8_t, SIDES * SIDES> old_heights;
-    std::array<uint64_t, SIDES> old_occ;
-
-    static move make_drop(int square) {
-        return {.expand = false, .height = 0, .square = square, .dir = 0, .old_heights = {}, .old_occ = {}};
+    static move make_drop(int square)
+    {
+        return { .expand = false, .square = square, .dir = NONE };
     }
 
-    static move make_normal(int square, int height, int dir) {
-        return {.expand = false, .height = height, .square = square, .dir = dir, .old_heights = {}, .old_occ = {}};
+    static move make_normal(int square, int height, int dir)
+    {
+        return { .expand = false, .square = square, .dir = dir };
     }
 
-    static move make_expand(int square, int dir) {
-        return {.expand = true, .height = 0, .square = square, .dir = dir, .old_heights = {}, .old_occ = {}};
+    static move make_expand(int square, int dir)
+    {
+        return { .expand = true,  .square = square, .dir = dir };
     }
 
-    static move none() {
-        return {.expand = false, .height = 0, .square = 64, .dir = 0, .old_heights = {}, .old_occ = {}};
+    static move none()
+    {
+        return { .expand = false, .square = 64, .dir = 0 };
     }
 
-    static bool valid_normal(int square, int dir) {
+    static bool valid_normal(int square, int dir)
+    {
         int row = square / SIZE;
         int col = square % SIZE;
-        switch (dir) {
-            case UP:
-                return row > 0;
-            case DOWN:
-                return row < (SIZE - 1);
-            case LEFT:
-                return col > 0;
-            case RIGHT:
-                return col < (SIZE - 1);
-            default:
-                std::cerr << "valid_normal failed\n";
-                exit(1);
+        switch (dir)
+        {
+        case UP:
+            return row > 0;
+        case DOWN:
+            return row < (SIZE - 1);
+        case LEFT:
+            return col > 0;
+        case RIGHT:
+            return col < (SIZE - 1);
+        default:
+            std::cerr << "valid_normal failed\n";
+            exit(1);
         }
     }
 
     int score() const { return _score; }
     bool is_none() const { return (square == 64); }
 
-    int type() const {
+    int type() const
+    {
         if (dir == NONE)
             return PLACE;
 
@@ -96,145 +99,207 @@ struct move {
 
     int col() const { return square % SIZE; }
 
-    int edge_distance() const {
-        switch (dir) {
-            case UP:
-                return row();
-            case DOWN:
-                return SIZE - row() - 1;
-            case LEFT:
-                return col();
-            case RIGHT:
-                return SIZE - col() - 1;
-            default:
-                std::cerr << "edge_distance failed\n";
-                exit(1);
+    int edge_distance() const
+    {
+        switch (dir)
+        {
+        case UP:
+            return row();
+        case DOWN:
+            return SIZE - row() - 1;
+        case LEFT:
+            return col();
+        case RIGHT:
+            return SIZE - col() - 1;
+        default:
+            std::cerr << "edge_distance failed\n";
+            exit(1);
         }
     }
 
-    void display() const {
-        switch (type()) {
-            case PLACE:
-                std::cout << "Place (" << row() + 1 << "/" << col() + 1 << ")";
-                break;
-            case NORMAL:
-                std::cout << "MOVE (" << row() + 1 << "/" << col() + 1 << "," << dir << ")";
-                break;
-            case EXPAND:
-                std::cout << "EXPAND (" << row() + 1 << "/" << col() + 1 << "," << dir << ")";
-                break;
-            default:
-                std::cerr << "display failed\n";
-                exit(1);
+    void display() const
+    {
+        switch (type())
+        {
+        case PLACE:
+            std::cout << "Place (" << row() + 1 << "/" << col() + 1 << ")";
+            break;
+        case NORMAL:
+            std::cout << "MOVE (" << row() + 1 << "/" << col() + 1 << "," << dir << ")";
+            break;
+        case EXPAND:
+            std::cout << "EXPAND (" << row() + 1 << "/" << col() + 1 << "," << dir << ")";
+            break;
+        default:
+            std::cerr << "display failed\n";
+            exit(1);
         }
     }
 };
 
-struct piece {
+struct piece
+{
     int side;
     int8_t height;
 };
 
-struct zobrist {
+struct zobrist
+{
     uint64_t pst[12][64][2];
     uint64_t stage[2];
     uint64_t side2move[2];
 };
 
-struct board {
-    std::array<int8_t, SIZE * SIZE> heights;
+struct board
+{
+    std::array<int8_t, SIZE* SIZE> heights;
     std::array<uint64_t, SIDES> occ;
 
     int side2move;
     int moves;
 
+    struct history
+    {
+        std::array<int8_t, SIZE* SIZE> heights;
+        std::array<uint64_t, SIDES> occ;
+    };
 
-    static board startpos() { return {.heights = {}, .occ = {}, .side2move = WHITE, .moves = 0}; }
+    int past_length;
+    std::array<history, DRAW_LENGTH + 1> past;
 
-    void make_move(move &m) {
-        switch (m.type()) {
-            case move::NORMAL: {
-                heights[m.to()] += heights[m.square];
-                heights[m.square] = 0;
-                occ[side2move] ^= (1ull << m.square);
-                occ[side2move] |= (1ull << m.to());
-                break;
-            }
-            case move::PLACE: {
-                heights[m.square] += PLACE_SIZE;
-                occ[side2move] |= (1ull << m.square);
-                break;
-            }
-            case move::EXPAND: {
-                // copy move
-                std::memcpy(m.old_heights.data(), heights.data(), heights.size() * sizeof(int8_t));
-                std::memcpy(m.old_occ.data(), occ.data(), occ.size() * sizeof(uint64_t));
+    static board startpos() { return { .heights = {}, .occ = {}, .side2move = WHITE, .moves = 0, .past_length = 0, .past = {} }; }
+
+    void make_move(move& m)
+    {
+        switch (m.type())
+        {
+        case move::NORMAL: {
+            past[past_length].heights[m.square] = heights[m.square];
+            past[past_length].heights[m.to()] = heights[m.to()];
+            std::memcpy(past[past_length].occ.data(), occ.data(), occ.size() * sizeof(uint64_t));
+
+            heights[m.to()] += heights[m.square];
+            heights[m.square] = 0;
+            occ[side2move] ^= (1ull << m.square);
+            occ[side2move] |= (1ull << m.to());
+            break;
+        }
+        case move::PLACE: {
+            heights[m.square] += PLACE_SIZE;
+            occ[side2move] |= (1ull << m.square);
+            break;
+        }
+        case move::EXPAND: {
+            // save
+            std::memcpy(past[past_length].heights.data(), heights.data(), heights.size() * sizeof(int8_t));
+            std::memcpy(past[past_length].occ.data(), occ.data(), occ.size() * sizeof(uint64_t));
 
 
-                std::array<int, SIZE> shifts{};
+            std::array<int, SIZE> shifts{};
 
-                int limit = m.edge_distance();
-                int power = (int) heights[m.square] - 1;
-                int before = power;
-                for (int step = 1; step <= limit; ++step) {
-                    int sq = m.square + m.dir * step;
-                    if ((occ[0] | occ[1]) & (1ull << sq)) {
-                        shifts[step] = before;
-                    } else {
-                        before -= 1;
-                        if (before == 0)
-                            break;
-                    }
+            int limit = m.edge_distance();
+            int power = (int)heights[m.square] - 1;
+            int before = power;
+            for (int step = 1; step <= limit; ++step)
+            {
+                int sq = m.square + m.dir * step;
+                if ((occ[0] | occ[1]) & (1ull << sq))
+                {
+                    shifts[step] = before;
                 }
+                else
+                {
+                    before -= 1;
+                    if (before == 0)
+                        break;
+                }
+            }
 
-                for (int step = limit; step >= 1; --step) {
-                    int sq = m.square + m.dir * step;
+            for (int step = limit; step >= 1; --step)
+            {
+                int sq = m.square + m.dir * step;
 
-                    int shift = shifts[step];
-                    if (shift > 0) {
-                        // try shift
-                        if (step + shift <= limit) {
-                            int shift_sq = m.square + m.dir * (step + shift);
+                int shift = shifts[step];
+                if (shift > 0)
+                {
+                    // try shift
+                    if (step + shift <= limit)
+                    {
+                        int shift_sq = m.square + m.dir * (step + shift);
 
-                            heights[shift_sq] = heights[sq];
-                            if (occ[0] & (1ull << sq)) {
-                                occ[0] |= (1ull << shift_sq);
-                            } else {
-                                occ[1] |= (1ull << shift_sq);
-                            }
+                        heights[shift_sq] = heights[sq];
+                        if (occ[0] & (1ull << sq))
+                        {
+                            occ[0] |= (1ull << shift_sq);
                         }
-
-                        // clear current
-                        heights[sq] = 0;
-                        occ[0] &= ~(1ull << sq);
-                        occ[1] &= ~(1ull << sq);
+                        else
+                        {
+                            occ[1] |= (1ull << shift_sq);
+                        }
                     }
 
-                    if (step <= power) {
-                        heights[sq] = 1;
-                        occ[side2move] |= (1ull << sq);
-                        occ[side2move ^ 1] &= ~(1ull << sq);
-                    }
+                    // clear current
+                    heights[sq] = 0;
+                    occ[0] &= ~(1ull << sq);
+                    occ[1] &= ~(1ull << sq);
                 }
 
-                heights[m.square] = 1;
-
-                break;
+                if (step <= power)
+                {
+                    heights[sq] = 1;
+                    occ[side2move] |= (1ull << sq);
+                    occ[side2move ^ 1] &= ~(1ull << sq);
+                }
             }
-            default:
-                std::cerr << "make_move failed\n";
-                exit(1);
+
+            heights[m.square] = 1;
+
+            break;
+        }
+        default:
+            std::cerr << "make_move failed\n";
+            exit(1);
         }
 
         side2move ^= 1;
         moves += 1;
+        past_length += 1;
     }
 
-    void unmake_move(const move &m) {
-        // TODO:
+    void unmake_move(const move& m)
+    {
+        past_length -= 1;
+        moves -= 1;
+        side2move ^= 1;
+
+        switch (m.type())
+        {
+        case move::NORMAL: {
+            heights[m.square] = past[past_length].heights[m.square];
+            heights[m.to()] = past[past_length].heights[m.to()];
+            std::memcpy(occ.data(), past[past_length].occ.data(), occ.size() * sizeof(uint64_t));
+            break;
+        }
+        case move::PLACE: {
+            heights[m.square] -= PLACE_SIZE;
+            if (heights[m.square] == 0)
+                occ[side2move] ^= (1ull << m.square);
+            break;
+        }
+        case move::EXPAND: {
+            std::memcpy(heights.data(), past[past_length].heights.data(), heights.size() * sizeof(int8_t));
+            std::memcpy(occ.data(), past[past_length].occ.data(), occ.size() * sizeof(uint64_t));
+
+            break;
+        }
+        default:
+            std::cerr << "unmake_move failed\n";
+            exit(1);
+        }
     }
 
-    int get_state() const {
+    int get_state() const
+    {
         if (moves < DROPS)
             return NONE;
 
@@ -247,37 +312,41 @@ struct board {
         return NONE;
     }
 
-    piece at(int index) const {
+    piece at(int index) const
+    {
         if (occ[0] & (1ull << index))
-            return {.side = 0, .height = heights[index]};
+            return { .side = 0, .height = heights[index] };
 
         if (occ[1] & (1ull << index))
-            return {.side = 1, .height = heights[index]};
+            return { .side = 1, .height = heights[index] };
 
-        return {.side = NONE, .height = 0};
+        return { .side = NONE, .height = 0 };
     }
 
     bool is_drop() const { return moves < DROPS; }
 
-    void display() const {
-        for (int i = 0; i < SIZE * SIZE; ++i) {
+    void display() const
+    {
+        for (int i = 0; i < SIZE * SIZE; ++i)
+        {
             piece p = at(i);
-            switch (p.side) {
-                case WHITE: {
-                    std::cout << "|" << RED << (std::to_string(p.height)) << RESET;
-                    break;
-                }
-                case BLACK: {
-                    std::cout << "|" << BLUE << (std::to_string(p.height)) << RESET;
-                    break;
-                }
-                case NONE: {
-                    std::cout << "| ";
-                    break;
-                }
-                default:
-                    std::cerr << "display failed\n";
-                    exit(1);
+            switch (p.side)
+            {
+            case WHITE: {
+                std::cout << "|" << RED << (std::to_string(p.height)) << RESET;
+                break;
+            }
+            case BLACK: {
+                std::cout << "|" << BLUE << (std::to_string(p.height)) << RESET;
+                break;
+            }
+            case NONE: {
+                std::cout << "| ";
+                break;
+            }
+            default:
+                std::cerr << "display failed\n";
+                exit(1);
             }
 
             if (i % SIZE == (SIZE - 1))
@@ -291,13 +360,16 @@ struct board {
     }
 };
 
-struct movegen {
-    const board &m_board;
+struct movegen
+{
+    const board& m_board;
 
-    std::vector<move> get_drops() {
+    std::vector<move> get_drops()
+    {
         uint64_t occ = ~m_board.occ[m_board.side2move ^ 1];
         std::vector<move> moves{};
-        while (occ) {
+        while (occ)
+        {
             int idx = __builtin_ctzll(occ);
             occ ^= (1ull << idx);
 
@@ -307,16 +379,20 @@ struct movegen {
         return moves;
     }
 
-    std::vector<move> get_normals() {
+    std::vector<move> get_normals()
+    {
         uint64_t occ = m_board.occ[m_board.side2move];
         std::vector<move> moves{};
-        while (occ) {
+        while (occ)
+        {
             int idx = __builtin_ctzll(occ);
             occ ^= (1ull << idx);
 
-            constexpr std::array<int, 4> dirs{move::UP, move::DOWN, move::LEFT, move::RIGHT};
-            for (int dir: dirs) {
-                if (move::valid_normal(idx, dir)) {
+            constexpr std::array<int, 4> dirs{ move::UP, move::DOWN, move::LEFT, move::RIGHT };
+            for (int dir : dirs)
+            {
+                if (move::valid_normal(idx, dir))
+                {
                     if (!(m_board.occ[m_board.side2move ^ 1] & (1ull << (idx + dir))) ||
                         m_board.heights[idx] > m_board.heights[idx + dir])
                         moves.push_back(move::make_normal(idx, m_board.heights[idx], dir));
@@ -327,27 +403,33 @@ struct movegen {
     }
 
 
-    std::vector<move> get_expands() {
+    std::vector<move> get_expands()
+    {
         uint64_t occ = m_board.occ[m_board.side2move];
         std::vector<move> moves{};
-        while (occ) {
+        while (occ)
+        {
             int idx = __builtin_ctzll(occ);
             occ ^= (1ull << idx);
 
-            if (m_board.heights[idx] == 1) {
+            if (m_board.heights[idx] == 1)
+            {
                 continue;
             }
 
-            constexpr std::array<int, 4> dirs{move::UP, move::DOWN, move::LEFT, move::RIGHT};
-            for (int dir: dirs) {
+            constexpr std::array<int, 4> dirs{ move::UP, move::DOWN, move::LEFT, move::RIGHT };
+            for (int dir : dirs)
+            {
                 moves.push_back(move::make_expand(idx, dir));
             }
         }
         return moves;
     }
 
-    std::vector<move> get_valids() {
-        if (m_board.is_drop()) {
+    std::vector<move> get_valids()
+    {
+        if (m_board.is_drop())
+        {
             return get_drops();
         }
 
